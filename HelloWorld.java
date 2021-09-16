66f68318acfdecae9468c14a52e9c3067c9fc7fe
Maria Dranker <musiasha@mail.ru>
2021-09-16 13:17:09 +0300

    Cherry-pick new-config to new-feature

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Hello World!1");
+=======
+        System.out.println("Hello World!\n");
+>>>>>>> dc1b0be (Change on HelloWorld.java)
 	System.out.println("New Feature!");
     }
 }
